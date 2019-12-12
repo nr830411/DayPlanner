e65094bdb3423f8a1b24460248193e8fc2a6d2a3
Nico Reed <nr830411@gmail.com>
2019-12-12 14:48:37 -0500

    (working application)

--- Assets/script.js
+++ Assets/script.js
@@ -9,4 +9,42 @@ $(document).ready(function() {
   });
 
   $("#todaysDate").text(moment().format("dddd, MMMM Do"));
+
+function hourUpdater() {
+  var currentHour = moment().hours();
+
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } 
+    else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } 
+    else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+}
+
+hourUpdater();
+
+
+var interval = setInterval(hourUpdater, 15000);
+
+$("#hour-9 .input").val(localStorage.getItem("hour-9"));
+$("#hour-10 .input").val(localStorage.getItem("hour-10"));
+$("#hour-11 .input").val(localStorage.getItem("hour-11"));
+$("#hour-12 .input").val(localStorage.getItem("hour-12"));
+$("#hour-13 .input").val(localStorage.getItem("hour-13"));
+$("#hour-14 .input").val(localStorage.getItem("hour-14"));
+$("#hour-15 .input").val(localStorage.getItem("hour-15"));
+$("#hour-16 .input").val(localStorage.getItem("hour-16"));
+$("#hour-17 .input").val(localStorage.getItem("hour-17"));
+
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DayPlanner
-a simple calendar application that allows the user to save events for each hour of the day
+a simple calendar application that allows the user to save events for each hour of a 9 to 5 work day
+
+1. 
+    entering the time of events within the text area allows local storage to save and categorize the data by clicking the save icons to the right of the designated text areas 
+
+2. 
+    to display the current hour the text area will display a red background with available/upcoming hours recieving a green background for that day. past hours recieve a greyed out text area. 
